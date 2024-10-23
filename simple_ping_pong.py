7e11d1c6322430cc9ff408e0d52674512f1c1b0b
Christ Chadrak MVOUNGOU <chadrakchristmvoungou@gmail.com>
2024-10-23 17:47:28 +0200

    Implementation of the Paddle class with its important methods. These two methods move up and move down are very important because it only the mouvements that our paddle will be able to do.

--- simple_ping_pong.py
+++ simple_ping_pong.py
@@ -39,3 +39,31 @@ class Ball(pygame.sprite.Sprite):
     def ball_bounce(self):
         self.velocity[0] = -self.velocity[0]
         self.velocity[1] = np.random.uniform(-4, 5)
+
+class Paddle(pygame.sprite.Sprite):
+
+    def __init__(self, color, width, height):
+        super().__init__()
+
+        self.image = pygame.Surface([width, height])
+        self.image.fill(WHITE_COULEUR)
+        self.image.set_colorkey(WHITE_COULEUR)
+        self.width = width
+        self.height = height
+        
+        pygame.draw.rect(self.image, color, [0, 0, self.width, self.height])
+
+        self.rect = self.image.get_rect()
+
+    def move_up(self, pixels):
+        self.rect.y -= pixels
+
+        # This test allows the ball to go out of the screen
+        if self.rect.y < 0:
+            self.rect.y = 0
+
+    def move_down(self, pixels):
+        self.rect.y += pixels
+
+        if self.rect.y > (HEIGHT - self.height):
+            self.rect.y = (HEIGHT - self.height)
